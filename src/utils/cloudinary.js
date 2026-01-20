497a2d1d6cbcbe7eeec7c58bafbbfdb4ecbb5ac0
vagabondraj <raviraj06112005@gmail.com>
2026-01-20 18:37:25 +0530

    Delete From Cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -27,5 +27,21 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 };
 
+const deleteFromCloudinary = async(imageUrl) => {
+    try{
+        if(!imageUrl) return;
 
-export {uploadOnCloudinary};
+        const publicId = imageUrl
+        .split("/")
+        .slice(imageUrl.split("/").indexOf("upload") + 2)
+        .join("/")
+        .split(".")[0];
+
+        const result = await cloudinary.uploader.destroy(publicId);
+        console.log("Cloudinary delete:", result);
+    }catch(error){
+        console.log("Cloudinary upload error:",error.message);
+    }
+}
+
+export {uploadOnCloudinary, deleteFromCloudinary};
