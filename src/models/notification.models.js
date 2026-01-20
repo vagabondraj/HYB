8676f1985dafe4ee5ae9c3d4abc42115b94a2252
vagabondraj <raviraj06112005@gmail.com>
2026-01-20 19:41:50 +0530

    Notification Model Added

---
+++ src/models/notification.models.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+    uer: {
+        type : mongoose.Schema.type.ObjectId,
+        ref: "User",
+        required: true
+    },
+    type: {
+        type : String,
+        enum : ['NEW_RESPONSE', 'MESSAGE', 'STATUS_UPDATE'],
+        required: true
+    },
+    request: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: "Request"
+    },
+    message: {
+        type:String,
+        required:true
+    },
+    isRead: {
+        type:Boolean,
+        default:false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {timestamps:true});
+
+notificationSchema.index({suer:1, isRead:1, createdAt: -1});
+export const Notification = mongoose.model('Notification', notificationSchema);
