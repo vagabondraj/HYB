1043ccbc9c0f2a2fa1b4f9d9da420e8afbace62f
vagabondraj <raviraj06112005@gmail.com>
2026-01-20 20:13:48 +0530

    Chat and Message model added

--- src/models/chat.models.js
+++ src/models/chat.models.js
@@ -1,2 +1,16 @@
 import mongoose from "mongoose";
 
+const chatSchema = new mongoose.Schema({
+    request: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Request",
+        required : true
+    },
+    participants : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required: true
+    }],
+}, {timestamps : true});
+
+export const Chat = mongoose.model("Chat", chatSchema);
